203e1af1db85304d71efc4d7e70ba63142288787
Babar124 <31736497+Babar124@users.noreply.github.com>
2017-12-20 13:12:34 +0500

    Add files via upload

---
+++ login.sql
@@ -0,0 +1,18 @@
+create database login_form
+
+use login_form
+
+create table login_tbl
+(
+id int primary key,
+Email varchar (20),
+Pass varchar (20)
+)
+
+select * from login_tbl
+
+insert into login_tbl values ('1','abbas3@gmail.com','abc')
+
+insert into login_tbl values ('2','abbas12@gmail.com','rocket')
+
+
